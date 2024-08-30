c8c84f0e582a589a5a676fbea6f74e35bb447f2a
skarthikeyan <skarthikeyan@PL-AU-PF3Z6R2T>
2024-08-30 10:12:29 +1000

    GetToken, ReadEnvironment variables in baseApi page

--- test/api/BaseApi.ts
+++ test/api/BaseApi.ts
@@ -1,10 +1,56 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { Agent } from "https";
-import Token from "./Token.js";
+import * as path from "path";
+import * as dotnet from "dotenv";
+
+function readEnvironmentVariables() {
+  const __dirname = path.resolve();
+        dotnet.config({
+            path : `${__dirname}/test/.env`
+        });
+        
+        const baseUrl = process.env.baseurl === undefined ? handleError("baseurl") : process.env.baseurl;
+        const customerbaseurl = process.env.customerbaseurl === undefined ? handleError("customerbaseurl") : process.env.customerbaseurl;
+        const client_id = process.env.client_id === undefined ? handleError("client_id") : process.env.client_id;
+        const client_secret = process.env.client_secret === undefined ? handleError("client_secret") : process.env.client_secret;
+        const scope = process.env.scope === undefined ? handleError("scope") : process.env.scope;
+        const grant_type = process.env.grant_type === undefined ? handleError("grant_type") : process.env.grant_type;
+        const tokenurl = process.env.tokenurl === undefined ? handleError("tokenurl") : process.env.tokenurl;
+
+        return {
+            baseUrl, customerbaseurl, client_id, client_secret, scope, grant_type, tokenurl
+        };
+}
+
+function handleError(envVariable : string) : void {
+    throw new Error(`No such '${envVariable}' variable present in the env file`);
+}
+
+async function getToken() {
+  console.log("I am into this main method");
+        let token = await axios.post(readEnvironmentVariables().tokenurl!, null, {
+            params : {
+                "grant_type" : readEnvironmentVariables().grant_type!,
+                "scope" : readEnvironmentVariables().scope!,
+                "client_secret" : readEnvironmentVariables().client_secret!,
+                "client_id" : readEnvironmentVariables().client_id!
+            },
+            headers : {
+                "Content-Type" : "application/x-www-form-urlencoded"
+            },
+            httpsAgent : new Agent({  
+                rejectUnauthorized: false
+            })
+        })
+        .then(response => response.data.access_token)
+        .catch(err => console.log(err));
+        console.log("Token:"+ token);
+        return token;
+    }
 
 const instance =  axios.create({
     headers : {
-        Authorization : `Bearer ${await Token.getToken()}`,
+        Authorization : `Bearer ${await getToken()}`, //
         Accept : "application/json"
     },
     httpsAgent : new Agent({
@@ -13,29 +59,29 @@ const instance =  axios.create({
 });
 
 instance.interceptors.response.use(
-    (res) => res,
-    (error: AxiosError) => {
-      const { data, status } = error.response!;
-      switch (status) {
-        case 400:
-          console.error(data);
-          break;
-  
-        case 401:
-          console.error('unauthorised');
-          break;
-  
-        case 404:
-          console.error('/not-found');
-          break;
-  
-        case 500:
-          console.error('/server-error');
-          break;
-      }
-      return Promise.reject(error);
+  (res) => res,
+  (error: AxiosError) => {
+    const { data, status } = error.response!;
+    switch (status) {
+      case 400:
+        console.error(data);
+        break;
+
+      case 401:
+        console.error('unauthorised');
+        break;
+
+      case 404:
+        console.error('/not-found');
+        break;
+
+      case 500:
+        console.error('/server-error');
+        break;
     }
-  );
+    return Promise.reject(error);
+  }
+);
 
 export default class BaseApi {
   
@@ -49,3 +95,5 @@ export default class BaseApi {
         getAll : async <T>(url: string) => await instance.get<T>(url).then(this.responseBody),
     }
 }
+
+
